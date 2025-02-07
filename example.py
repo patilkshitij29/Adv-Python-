10cbb7b7aadf43d1bed227eee805cc779dda7218
patilkshitij29 <patilkshitij29@gmail.com>
2025-02-07 15:41:44 +0530

    New Function Example

--- example.py
+++ example.py
@@ -1,5 +1,25 @@
-def add(x,y):
-    z = x+y
-    return z
-a = add(10,20)
-print("So the output is :",a)
+# Calculate Simple Intrest
+
+def simple_intrest(p:float|int , n:int , r: float|int) -> tuple:
+
+    """
+    p : Principal in INR
+    n : Number of years
+    r : Rate of intrest in percent per annum
+    output intrest, amount
+    
+    """
+
+    i = (p*n*r)/100
+    a = p + i
+    return i,a
+
+p = float(input("Enter the value of Principal in INR :"))
+n = int(input("Enter Number of Years :"))
+r = float(input("Enter rate of intrest %p.a :"))
+
+i , a = simple_intrest(p,n,r)
+
+print(f"Simple Intrest : {i:.2f} INR")
+print(f"Amount : {a:.2f} INR")
+
